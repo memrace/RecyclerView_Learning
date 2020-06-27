97c19349ed42ce82a2e4eeb2807715759b729395
robbier-sozo <sozonovalexander13@gmail.com>
2020-06-27 11:33:25 +0300

    Implementing methods of Adapter

---
+++ app/src/main/java/com/android/example/recyclerview_learning/ListSelectionViewHolder.kt
@@ -0,0 +1,7 @@
+package com.android.example.recyclerview_learning
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class ListSelectionViewHolder(itemView:View): RecyclerView.ViewHolder(itemView) {
+}
--- app/src/main/java/com/android/example/recyclerview_learning/MainActivity.kt
+++ app/src/main/java/com/android/example/recyclerview_learning/MainActivity.kt
@@ -2,10 +2,21 @@ package com.android.example.recyclerview_learning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+	lateinit var listsRecyclerView: RecyclerView
+
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+
+// RecyclerView
+		listsRecyclerView = findViewById(R.id.recyclerView)
+		listsRecyclerView.layoutManager = LinearLayoutManager(this)
+		listsRecyclerView.adapter = ListSelectionRecyclerViewAdapter()
 	}
 }
